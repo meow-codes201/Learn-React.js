573dbea70ca3d0a90c82adcca2efe122ba940f58
meow-codes201 <humairat291@gmail.com>
2025-11-27 08:16:59 +0530

    propTypes in js

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,13 +2,14 @@ import Header from "./Header"
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
+import Student from "./Student";
 function App() {
 return(
 <>
-<Card></Card>
-<Card></Card>
-<Card></Card>
-<Card></Card>
+    <Student name="Spongebob" age={30} isStudent={true}></Student>
+    <Student name='Patrick' age={42} isStudent={false}></Student>
+    <Student name="Squidward" age={50} isStudent= {false}></Student>
+    <Student name="Sandy" age={27} isStudent={true}></Student>
 
 </>
 );
---
+++ my-react-app/src/Student.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import './Student.css'
+function Student(props){
+
+    return(
+        <div className="student">
+            <p>Name:{props.name}</p>
+            <p>Age:{props.age}</p>
+            <p>Student: {(props.isStudent)?"yes":"no"}</p>
+        </div>
+    );
+}
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
+}
+
+
+
+export default Student
