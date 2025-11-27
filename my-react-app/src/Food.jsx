dc13cd28005d4138ccd5e6b7e89bc68ac6b0cca2
meow-codes201 <humairat291@gmail.com>
2025-11-27 07:11:50 +0530

    Clean repo with new gitignore

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,17 @@
+function Food(){
+    const food1 = "Apple";
+    const food2 = "Fish";
+
+    return(
+        <ul>
+            <li>Watermelon</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+
+    );
+
+
+
+}
+export default Food 
