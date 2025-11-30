65a357a33b61d6aec19344229b7c8d46be7152c5
meow-codes201 <humairat291@gmail.com>
2025-11-30 13:58:11 +0530

    clean App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,41 +1,7 @@
-import Header from "./Header"
-import Footer from "./Footer";
-import Food from "./Food";
-import Card from "./Card";
-import Student from "./Student";
-import UserGreeting from "./UserGreeting";
-import List from "./List";
-import Button from "./Button";
-import ProfilePicture from "./ProfilePicture";
-import Counter from "./Counter";
-import MyComponent from "./MyComponent";
-import ColorPicker from "./colorPicker";
-import UpdaterFunction from "./updaterFunction";
-import UpdateObjects from "./UpdateObjects";
-import UpdateArrays from "./UpdateArrays";
-import ArrayOfObjects from "./ArrayOfObjects";
-import ToDoList from "./ToDoList";
-import UseEffectHook from "./useEffectHook";
-import MyEffectHook from "./MyEffectHook";
-import DigitalClock from "./DigitalClock";
-import CompopentA from "./CompopentA";
-import Stopwatch from "./Stopwatch";
-import Ref from "./Ref";
 function App() {
 return(
 <>
-<Stopwatch></Stopwatch>
-{/* <Ref></Ref> */}
-{/* <CompopentA></CompopentA> */}
-{/* <DigitalClock></DigitalClock> */}
-{/* <MyEffectHook></MyEffectHook> */}
-{/* <UseEffectHook></UseEffectHook> */}
-{/* <ToDoList></ToDoList> */}
-{/* <ArrayOfObjects></ArrayOfObjects> */}
-{/* <UpdateArrays></UpdateArrays> */}
-{/* <UpdateObjects></UpdateObjects> */}
-{/* <UpdaterFunction></UpdaterFunction> */}
-{/* <ColorPicker></ColorPicker> */}
+
 
 </>
 );
@@ -44,28 +10,3 @@ return(
 
 export default App
 
-
-// {/* <MyComponent></MyComponent> */}
-// {/* <Counter></Counter> */}
-// {/* <ProfilePicture></ProfilePicture> */}
-// {/* <Button></Button> */}
-//  {/* {fruits.length>0?<List items={fruits} category="Fruits"></List>:null};
-//     {veg.length>0&&<List items={veg} category="Vegetables"></List>} */}
-//     {/* <UserGreeting isLoggedIn={true} username="Omnis"></UserGreeting> */}
-//     {/* <Student name="Spongebob" age={30} isStudent={true}></Student>
-//     <Student name='Patrick' age={42} isStudent={false}></Student>
-//     <Student name="Squidward" age={50} isStudent= {false}></Student>
-//     <Student name="Sandy" age={27} isStudent={true}></Student> */}
-
-//   const fruits = [{id:1,name:'apple',calories:95},
-//                 {id:2,name:'orange',calories:"45"},
-//                 {id:3,name:'banana',calories:105},
-//                 {id:4,name:'coconut',calories:159},
-//                 {id:5,name:'pineapple',calories:37}
-// ];
-// const veg = [{id:6,name:'celery',calories:15},
-//                 {id:7,name:'potatoes',calories:110},
-//                 {id:8,name:'carrots',calories:25},
-//                 {id:9,name:'corn',calories:63},
-//                 {id:10,name:'broccoli',calories:50}
-// ];
