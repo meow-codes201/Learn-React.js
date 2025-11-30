abff66f4fb735f4d22508b9996dc2bfa15c626ec
meow-codes201 <humairat291@gmail.com>
2025-11-30 10:07:35 +0530

    useRef() in react hooks

--- my-react-app/src/Ref.jsx
+++ my-react-app/src/Ref.jsx
@@ -1,18 +1,45 @@
 import React, {useState,useEffect,useRef} from "react"
 import Button from "./Button";
 function Ref(){
-const ref = useRef(0);
+const inputRef1 = useRef(null);
+const inputRef2 = useRef(null);
+const inputRef3 = useRef(null);
+
+
 useEffect(()=>{
-    console.log('rendered');
+    console.log('component rendered');
+    // console.log(inputRef/);
     
 })
-function handleClick(){
-    console.log(ref.current); 
-    ref.current =ref.current + 1;
-    
+function handleClick1(){
+    inputRef1.current.focus();
+    // ref.current =ref.current + 1;
+    // console.log(ref.current); 
+    inputRef1.current.style.backgroundColor = 'green'
+}
+function handleClick2(){
+    inputRef2.current.focus();
+    // ref.current =ref.current + 1;
+    // console.log(ref.current); 
+    inputRef2.current.style.backgroundColor = 'green'
+}
+function handleClick3(){
+    inputRef3.current.focus();
+    // ref.current =ref.current + 1;
+    // console.log(ref.current); 
+    inputRef3.current.style.backgroundColor = 'green'
 }
 return(<>
-<button onClick={handleClick}>click</button>
+<button onClick={handleClick1}>click1</button>
+<input type="text" ref={inputRef1} />
+
+<button onClick={handleClick2}>click2</button>
+<input type="text" ref={inputRef2} />
+
+<button onClick={handleClick3}>click3</button>
+<input type="text" ref={inputRef3} />
+
+
 </>)
 
 }
