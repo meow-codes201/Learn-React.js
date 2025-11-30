c509880d9e9a0206367966ddd4ef18d50b3bb703
meow-codes201 <humairat291@gmail.com>
2025-11-30 22:05:58 +0530

    react facts

--- my-react-app/src/ReactFacts.jsx
+++ my-react-app/src/ReactFacts.jsx
@@ -1,11 +1,20 @@
 import styles from './ReactFacts.module.css';
-import picLink from  './assets/nebula.jpg'
+import picLink from  './assets/react.svg'
 function ReactFacts(){
 const link = "./assets./ChatGPT Image Nov 29, 2025, 07_29_36 PM.png";
 return(
     <>
-<h1>This is React</h1>00
-<img src={picLink} alt="" />
+<img src={picLink} alt="react-logo" width={150} />
+
+<h1>This is React</h1>
+<ul>
+<li>first released in 2013</li>
+<li>was originally created by Jordan Walke</li>
+<li>Has well over 200k stars on Github</li>
+<li>is maintained by Meta</li>
+<li>Powers thousands of enterprise apps, including mobile apss</li>
+
+</ul>
 
 
 </>
