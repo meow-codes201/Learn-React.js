aff2587cf28a34d5ecac49b8944ceb59db8822a4
meow-codes201 <humairat291@gmail.com>
2025-12-01 09:58:55 +0530

    excitement for react js

--- my-react-app/src/Excitement.jsx
+++ my-react-app/src/Excitement.jsx
@@ -1,12 +1,22 @@
 import picLink from  './assets/react.svg'
 import { Fragment } from 'react'
+import styles from './Excitement.module.css';
 function Excitement(){
 
     return(
         <Fragment>
-        <img src={picLink} alt="react-logo" width={160} />
+        <img src={picLink} alt="react-logo" className={styles['react-logo']} />
+        <header className={styles["head"]}>
+            <nav>
+            <ul className={styles['nav-list']}>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        </header>
 
-        <ol>
+        <ol className={styles['my-ol']}>
             <li>new skill</li>
             <li>popular library</li>
             <li>easy</li>
