a6517e858287c85e3fba29de45bcac9cd05df9d7
meow-codes201 <humairat291@gmail.com>
2025-12-05 09:15:40 +0530

    Header component in react

---
+++ my-react-app/src/TravelJournal/HeaderTravel.jsx
@@ -0,0 +1,18 @@
+import pic from "./globeopacity.png"
+import styles from "./HeaderTravel.module.css";
+function HeaderTravel() { 
+
+
+    return(
+<>
+<header className={styles.hr1}>
+        <h1>My Travel Journal</h1>
+
+</header>
+
+
+
+</>
+    );
+}
+export default HeaderTravel;
