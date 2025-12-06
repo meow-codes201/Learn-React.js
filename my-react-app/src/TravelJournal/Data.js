7204611650f6dca8493f8e02233b8cbf06047df0
meow-codes201 <humairat291@gmail.com>
2025-12-06 09:24:53 +0530

    export travel journal data

---
+++ my-react-app/src/TravelJournal/Data.js
@@ -0,0 +1,57 @@
+import fuji from "./Mt._Fuji.jpg"
+import sydney from "./Sydney_Opera_House.jpg"
+import newyork from "./new_york.jpg"
+import dubai from "./Dubai_Marina_Skyline.jpg"
+export default [
+{
+    id:1,
+    img:{
+        src:fuji,
+        alt:"Mount Fuji"
+    },
+    title:"Mount Fuji",
+    country:"JAPAN",
+    googleMapsUrl:"https://maps.app.goo.gl/hz4cWfUyGue7o1Xw6",
+    date:"12 Jan, 2023 - 24 Jan, 2023",
+    description:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,389 feet). It is an active stratovolcano that last erupted in 1707-1708. Mount Fuji is a popular destination for tourists and climbers, known for its symmetrical cone shape and cultural significance."
+},
+{
+    id:2,
+    img:{
+        src:sydney,
+        alt:"Sydney Opera House"
+},
+    title:"Sydney Opera House",
+    country:"AUSTRALIA",
+    googleMapsUrl:"https://maps.app.goo.gl/1JtXGZ4gYhH2",
+    date:"27 May, 2023 - 8 Jun, 2023",
+    description:"The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+},
+{
+    id:3,
+    img:{
+        src:newyork,
+        alt:"New York City"
+    },
+    title:"New York City",
+    country:"USA",
+    googleMapsUrl:"https://maps.app.goo.gl/8b5YH5J6v3Q2",
+    date:"01 Oct, 2023 - 18 Nov, 2023",
+    description:"New York City (NYC), often simply called New York, is the most populous city in the United States. With an estimated 2019 population of 8,336,817 distributed over about 302.6 square miles (784 km2), New York is also the most densely populated major city in the United States."
+    
+    
+},
+{
+    id:4,
+    img:{
+        src:dubai,
+        alt:"Dubai Marina"
+    },
+    title:"Dubai Marina",
+    country:"UAE",
+    googleMapsUrl:"https://maps.app.goo.gl/XYZ123456789",
+    date:"05 Dec, 2023 - 20 Dec, 2023",
+    description:"Dubai Marina is an artificial canal city, built along a two-mile (3 km) stretch of Persian Gulf shoreline. It is known for its modern skyline, luxurious lifestyle, and vibrant nightlife."
+}
+
+]
