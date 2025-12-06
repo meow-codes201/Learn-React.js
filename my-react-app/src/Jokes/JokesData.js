df6c785c97a55b5dc2d990fa459e8d497018b5c4
meow-codes201 <humairat291@gmail.com>
2025-12-06 09:22:29 +0530

    export jokes data

---
+++ my-react-app/src/Jokes/JokesData.js
@@ -0,0 +1,22 @@
+export default [
+{
+    setup: "Why did the scarecrow win an award?",
+    punchline: "Because he was outstanding in his field!"
+},
+{
+    setup: "Why don't scientists trust atoms?",
+    punchline: "Because they make up everything!"
+},
+{
+    setup: "Why did the bicycle fall over?",
+    punchline: "Because it was two-tired!"
+},
+{
+    setup: "What do you call fake spaghetti?",
+    punchline: "An impasta!"
+},
+{
+    setup: "Why did the math book look sad?",
+    punchline: "Because it had too many problems."
+}
+]
